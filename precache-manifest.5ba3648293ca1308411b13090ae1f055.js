367a505c578208d646237498dab51858308e7d74
Edwin Chia <starlest@gmail.com>
2019-07-14 14:56:00 +0700

    Updates

---
+++ precache-manifest.5ba3648293ca1308411b13090ae1f055.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0895bdc679d2199367540295541d1438",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5e7512e6e9758d496b94",
+    "url": "/monsters-rolodex/static/css/main.edbef255.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5e7512e6e9758d496b94",
+    "url": "/monsters-rolodex/static/js/main.a4e35626.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
